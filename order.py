8a38da1198c25651080176ad00f54442b6eda1a8
Comicly <93946611+Comicly69@users.noreply.github.com>
2023-02-01 18:46:37 +0000

    njasndjnasjdn

--- order.py
+++ order.py
@@ -1,1 +1,50 @@
-print("Hello world")
+import random
+
+menu = {
+    "burger": 3.99,
+    "quarter pounder": 4.99,
+    "nuggets": 4.49,
+    "fries": 2.49,
+    "drink": 1.99
+}
+
+def order_item(item, quantity):
+    price = menu[item] * quantity
+    return f"{quantity} {item}(s) - ${price:.2f}"
+
+def place_order(total_price, items_ordered):
+    print("Menu:")
+    for item, price in menu.items():
+        print(f"{item}: ${price:.2f}")
+
+    item = input("\nWhat would you like to order?\n ")
+    if item.lower() not in menu:
+        print(f"{item} is not on the menu.")
+        return place_order(total_price, items_ordered)
+    item_key = next((key for key, value in menu.items() if item.lower() == key), None)
+    quantity = input("How many would you like to order? ")
+    try:
+        quantity = int(quantity)
+    except ValueError:
+        print("Invalid quantity. Please try again.")
+        return place_order(total_price, items_ordered)
+    item_price = menu[item_key] * quantity
+    total_price += item_price
+    items_ordered.append(order_item(item_key, quantity))
+    more = input("Would you like to order more? (y/n) ")
+    if more.lower() == 'y':
+        return place_order(total_price, items_ordered)
+    else:
+        return total_price, items_ordered
+
+total_price, items_ordered = place_order(0, [])
+print("Order Summary:")
+for item in items_ordered:
+    print(item)
+print(f"Total: ${total_price:.2f}")
+
+order_number = random.randint(1, 999)
+time_to_wait = random.randint(1, 10)
+print(f"Order Number: {order_number}")
+print(f"Your meal will be ready in approximately {time_to_wait} minutes.")
+print("Please proceed to the counter when your order number is called.")
