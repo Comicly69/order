b488f5abd168f33b053e4a0205d689b0768dba76
Comicly <93946611+Comicly69@users.noreply.github.com>
2023-02-02 20:16:16 -0500

    add shit

--- test.py
+++ test.py
@@ -1,5 +1,26 @@
-import datetime
-today = datetime.datetime.now().weekday()
-days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
-saleday = days[today]
-print(saleday)
+discount = 0
+codes = ["xigY9jo5bA", "fK7sfOr3J2"]
+discounts = [0.3, 0.25]
+
+answer = input("Would you like to enter a discount code (y/n)? ")
+
+if answer.lower() == "y":
+    code = input("Please enter your discount code: ")
+    with open("C:\\Users\\comic\\Documents\\GitHub\\order\\assets\\discounts\\discounts.txt", "r") as file:
+        for line in file:
+            for c, d in zip(codes, discounts):
+                if line.strip() == code:
+                    discount = d
+                    break
+            if discount != 0:
+                break
+
+    if discount != 0:
+        print("Discount applied:", discount)
+    else:
+        print("Discount code not found")
+else:
+    print("No discount being applied")
+
+# Placeholder for sending order
+print("Sending order")
