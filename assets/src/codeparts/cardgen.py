cc5d913c00bfa3f313534bbd6ec7642fb310ab69
Comicly <93946611+Comicly69@users.noreply.github.com>
2023-02-03 18:56:19 +0000

    commit

--- assets/src/codeparts/cardgen.py
+++ assets/src/codeparts/cardgen.py
@@ -1,3 +1,10 @@
 import ccard
+import random
+import string
 
-print (ccard.visa())
+print ("VISA:" + ccard.visa())
+print ("MASTER:" + ccard.mastercard())
+print ("AMEX:" + ccard.americanexpress())
+print ("DISCOVER:" + ccard.discover())
+genisis = ''.join(random.choices(string.ascii_letters + string.digits, k=12))
+print ("Gensis ID:" + genisis)
--- dev/devmode.py
+++
@@ -1,3 +0,0 @@
-import ccard
-
-card = ccard.visa()
